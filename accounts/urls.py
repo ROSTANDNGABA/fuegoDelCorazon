7255a89e5c95f482df18b520971e76ddf4a12e9c
ROSTANDNGABA <ROSTANDNGABA@GMAIL.COM>
2026-02-13 18:54:48 +0100

    Create simple login template to fix 500 error

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
+    path('login/', LoginView.as_view(template_name='accounts/login_simple.html'), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('create-profile/', views.create_profile, name='create_profile'),
